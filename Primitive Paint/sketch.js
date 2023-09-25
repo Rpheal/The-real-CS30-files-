a5053a086b1b85b697977e6c699e0ccdef553be8
Rpheal <Raphealoki@gmail.com>
2023-09-25 12:47:14 -0600

    Autonomous art function added
    Trying to create a function that works on its own irrespective of the user's interactions.

--- Primitive Paint/sketch.js
+++ Primitive Paint/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Prrimitive Paint Assignment 
+// Rapheal Oki
+// 26-09-2023
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -16,6 +16,7 @@ let authorName = "Rapheal";
 
 
 
+
 function generateRandomColor(){
 // generate hex format for random colors
   let hex = "#" + Math.floor(Math.random()*16777215).toString(16);
@@ -26,8 +27,8 @@ function draw() {
   textSize(32);
   textFont("Courier New");
   fill(0);
-  text(authorName,0,950);
-
+  text(authorName, width/2, (height/4) - 50);
+  // autonomousArt();
   
 }
 
@@ -48,4 +49,15 @@ function keyTyped() {
     fill(generateRandomColor());
     triangle(mouseX,mouseY,mouseX+40,mouseY+40,mouseX-40,mouseY+40);
   }
+}
+
+
+
+function autonomousArt(){
+  // Creating a visual element that works independently of the user's interaction
+  
+  // while (true) {
+  //   f = fill(generateRandomColor());
+  // }
+  // circle(40,40,50);
 }
