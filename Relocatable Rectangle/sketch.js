2f8f7a914dec48dc1738f9c03719f245602a8d2f
Rpheal <86891223+Rpheal@users.noreply.github.com>
2023-09-27 12:00:31 -0600

    upadte

--- Relocatable Rectangle/sketch.js
+++ Relocatable Rectangle/sketch.js
@@ -10,6 +10,9 @@
 // Global variable
 let x,y, rHeight, rWidth; //(x,y) will be center referenced.
 let rLeft, rRight, rTop, rBottom;   // edge positions of our rectangle
+let mouseOver = false; // are we hovering over the rectangle
+let pickedUp = false; // are we currently moving the object
+let offX,offY; // so object does not snap to mouse
 
 
 function setup() {
@@ -25,7 +28,7 @@ function setup() {
 function updateEdgePositions(){
   // update the right/left/top/bottom of our rectangle
   rLeft = x - rWidth/2; 
-  rHeight = x + rWidth/2;
+  rRight = x + rWidth/2;
   rTop = y - rHeight/2;
   rBottom = y + rHeight/2;
 }
@@ -34,9 +37,43 @@ function drawRectangle(){
   // render rectangle and checks for mouse interactions
   updateEdgePositions();
   print(rLeft + " " + rRight + " " + rTop + " " + rBottom);
+
+
+  // Are we in the region
+  if (mouseX>rLeft && mouseX<rRight && mouseY>rTop && mouseY<rBottom) {
+    fill(70,230,54);
+    mouseOver = true;
+  }
+  
+  else{
+    fill(30,70,170);
+    mouseOver = false;
+  }
+
+  if (pickedUp){
+    x = mouseX - offX;
+    y = mouseY - offY;
+  }
+
+  // finally drawing the triangle
+  rect(x,y,rWidth,rHeight);
 }
 
 function draw() {
   background(220);
   drawRectangle();
+}
+
+function mousePressed(){
+  if (mouseOver){
+    pickedUp = true;
+    offX = mouseX - x;
+    offY = mouseY - y;
+  }
+
+}
+
+
+function mouseReleased(){
+  pickedUp = false;
 }
