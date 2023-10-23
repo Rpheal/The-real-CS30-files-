c50b8b3b2e6014950dcd0468d6f395c4b6f53b29
Rpheal <86891223+Rpheal@users.noreply.github.com>
2023-10-23 12:10:22 -0600

    update on Car assignment

--- Cars Assigment/sketch.js
+++ Cars Assigment/sketch.js
@@ -6,6 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
+let eastbound = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
@@ -14,15 +17,63 @@ function setup() {
 function draw() {
   background(200);
   drawRoad();
+  for (let i = 0; i<eastbound.length; i++){
+    eastbound[i].action();
+  }
 }
 
+function mouseClicked(){
+  eastbound.push(new Vehicle(mouseX, mouseY, 1));
+}
 
 function drawRoad(){
   fill(0);
+  noStroke();
   rect(width/2,height/2,width,height/1.9);
-  stroke(255, 255, 0);
-  strokeWeight(6);
   for(let i=0; i < width; i+=100){
-    line(i,height/2,i+50  ,height/2);
+    stroke(255, 255, 0);
+    strokeWeight(6);
+    line(i,height/2,i+50,height/2);
+
+  }
+}
+
+
+class Vehicle{
+  // constructor
+  constructor(x,y,direction){
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.c = color(random(255),random(255),random(255));
+    this.type = int(random(2));
+  }
+
+  action(){
+    // call the other functions like move, 
+    this.display();
+  }
+
+  display(){
+    if(this.type===0){
+      this.drawCar();  // 0 -car
+    }
+
+    else if (this.type===1){ // 1 - car
+      this.drawTruck();
+    }
+  }
+
+
+  drawCar(){
+    fill(this.c);
+    rect(this.x,this.y,100,100);
+
+  }
+
+  drawTruck(){
+    fill(this.c);
+    ellipse(this.x,this.y,100,40);
+
   }
 }
