610e80f59b9bd77b104a3e4ee148afe473382a7b
Rpheal <86891223+Rpheal@users.noreply.github.com>
2023-12-12 12:08:22 -0600

    Button Click to start making money
    Today, Alvin worked on the first game requirement while Rapheal worked on th e proposal and Handed that in.

--- Captsone Project CS30/sketch.js
+++ Captsone Project CS30/sketch.js
@@ -1,15 +1,48 @@
-// Bus
-// Your Name
-// Date
+// Bussiness Similator
+// Rapheal Oki & Alvin Shen
+// 12/11/2023
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let score = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  textSize(32);
 }
+
 
 function draw() {
   background(220);
+  fill(0);
+  text(`Score: ${score}`, width / 2, height / 2);
+
+
+  // Button
+  fill(150, 200, 255);
+  rect(width / 2 - 50, height / 2 + 50, 100, 50);
+  fill(0);
+  text("Click me!", width / 2, height / 2 + 75);
+}
+
+
+function mouseClicked() {
+  // Check if the mouse is over the button
+  if (
+    mouseX > width / 2 - 50 &&
+    mouseX < width / 2 + 50 &&
+    mouseY > height / 2 + 50 &&
+    mouseY < height / 2 + 100
+  ) {
+    // Increment the score when the button is clicked
+    score++;
+  }
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
