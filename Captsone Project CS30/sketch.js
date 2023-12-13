f4890fc63f58f4f210375b3493510492af22d2aa
Rpheal <Raphealoki@gmail.com>
2023-12-13 12:24:14 -0600

    Gathering Assets
    Alvin and I worked on Gathering assets for the project

--- Captsone Project CS30/sketch.js
+++ Captsone Project CS30/sketch.js
@@ -3,7 +3,7 @@
 // 12/11/2023
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// -
 
 let score = 0;
 
@@ -18,12 +18,13 @@ function setup() {
 function draw() {
   background(220);
   fill(0);
-  text(`Score: ${score}`, width / 2, height / 2);
+  text(`Money: ${score}`, width / 2, height / 2);
 
 
   // Button
   fill(150, 200, 255);
-  rect(width / 2 - 50, height / 2 + 50, 100, 50);
+  rectMode(CORNER);
+  rect(width / 2 - 80, height / 2 + 50, 160, 50, 15);
   fill(0);
   text("Click me!", width / 2, height / 2 + 75);
 }
@@ -32,8 +33,8 @@ function draw() {
 function mouseClicked() {
   // Check if the mouse is over the button
   if (
-    mouseX > width / 2 - 50 &&
-    mouseX < width / 2 + 50 &&
+    mouseX > width / 2 - 80 &&
+    mouseX < width / 2 + 80 &&
     mouseY > height / 2 + 50 &&
     mouseY < height / 2 + 100
   ) {
