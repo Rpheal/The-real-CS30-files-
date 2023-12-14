925c2fa0eb93fbbb76a9ecae0c0167c5231b1416
Rpheal <Raphealoki@gmail.com>
2023-12-14 12:13:21 -0600

    Worked on the Stocked Purchase
    Worked on the Stocked Purchase and changed the name of our assets for east access and rendering

--- Captsone Project CS30/sketch.js
+++ Captsone Project CS30/sketch.js
@@ -1,24 +1,30 @@
-// Bussiness Similator
+// Business Simulator
 // Rapheal Oki & Alvin Shen
 // 12/11/2023
 //
 // Extra for Experts:
 // -
 
-let score = 0;
+let score = 0; let balance = 10000; let stockPrice = 100;
+let stocksOwned = 0;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
   textSize(32);
+  // createButton('Buy').mousePressed(buyStock);
+  // createButton('Sell').mousePressed(sellStock);
+
 }
 
 
 function draw() {
   background(220);
   fill(0);
+  displayInfo();
   text(`Money: ${score}`, width / 2, height / 2);
+  
 
 
   // Button
@@ -47,3 +53,41 @@ function mouseClicked() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+
+function displayInfo() {
+  fill(0);
+  textSize(16);
+  text(`Balance: $${balance.toFixed(2)}`, width/3, 30);
+  text(`Stock Price: $${stockPrice.toFixed(2)}`, width/3, 60);
+  text(`Stocks Owned: ${stocksOwned}`, width/3, 90);
+  text(`Portfolio Value: $${(balance + stocksOwned * stockPrice).toFixed(2)}`, width/3, 120);
+}
+
+
+function buyStock() {
+  if (balance >= stockPrice) {
+    stocksOwned++;
+    balance -= stockPrice;
+    updateStockPrice();
+  } else {
+    alert('Not enough balance to buy stocks.');
+  }
+}
+
+
+function sellStock() {
+  if (stocksOwned > 0) {
+    stocksOwned--;
+    balance += stockPrice;
+    updateStockPrice();
+  } else {
+    alert('No stocks to sell.');
+  }
+}
+
+
+function updateStockPrice() {
+  stockPrice = random(80, 120);
+}
+
