e328ad238623e24b7dde02e3f229d700ecd9a572
Rpheal <Raphealoki@gmail.com>
2023-12-30 23:21:06 -0600

    starting the balloon tree assignemt

---
+++ The Balloon Tree/sketch.js
@@ -0,0 +1,17 @@
+// The Balloon Tree
+// Rapheal Oki
+// 30/12/2023
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
